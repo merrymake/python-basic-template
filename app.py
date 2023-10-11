2a5a9ad21b7fd8bb5ace7172dc46d9e083f74c11
Christian Clausen <maxclausen@gmail.com>
2023-10-11 12:15:25 +0200

    Pull Python basic template

---
+++ app.py
@@ -0,0 +1,16 @@
+import sys
+
+from merrymake import Merrymake
+from merrymake.merrymimetypes import MerryMimetypes
+from merrymake.envelope import Envelope
+
+def handleHello(payloadBytes: bytearray, envelope: Envelope):
+    payload = bytes(payloadBytes).decode('utf-8')
+    Merrymake.reply_to_origin(f"Hello, {payload}!", MerryMimetypes.getMimeType("txt"));
+
+def main():
+    (Merrymake.service()
+        .handle("handleHello", handleHello));
+
+if __name__ == "__main__":
+    main()
